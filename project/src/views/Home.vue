5d8c8872c8358fa9a5ab6bc04b383bcd7b810ae7
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-03-15 13:29:16 -0400

    d

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,15 +1,7 @@
 <template>
   
   <form @submit.prevent="handleSubmit">
-    <h3>Login</h3>
-
-    <label for="email">Email:</label>
-    <input type="email" name="email" v-model="email" required>
-
-    <label for="email">Password:</label>
-    <input type="password" name="password" v-model="password" required>
-
-    <button>Login</button>
+  
   </form>
 
 </template>
