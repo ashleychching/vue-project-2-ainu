5913649f27a6b6e043b71e6190a4729a2256762c
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-03-25 13:53:26 -0400

    omg

--- project/src/views/MobileLogin.vue
+++ project/src/views/MobileLogin.vue
@@ -1,7 +1,19 @@
 <template>
- <div class="desktopView">
+ <div class="mobilePortraitView">
+      <div class="topWrap">
+        <img src="@/assets/logo.svg" alt="logo" id="logo" />
+        <div class="themeWrapper">
+          <input
+            type="checkbox"
+            name="checkbox"
+            class="togThemer"
+            @change="switchTheme()"
+          />
+        </div>
+      </div>
+      <h1 class="welcome" style="margin-top: 40px">Hello</h1>
+      <h2 class="headline">Join kishu today.</h2>
       <div class="sign-box">
-        <h1 class="welcome" style="margin-top: 40px">Happening now.</h1>
         <button class="facebook-signup">
           <img src="@/assets/facebook.png" alt="fb" class="fb-logo" />
           <p class="btn-text">Sign in with Facebook</p>
@@ -10,7 +22,9 @@
           <img src="@/assets/google.png" alt="goog" class="goog-logo" />
           <p class="btn-text">Sign in with Google</p>
         </button>
-        <button class="email-signup" @click="goToEmail()" >Sign up with email</button>
+        <button class="email-signup" @click="goToEmail()">
+          Sign up with email
+        </button>
         <p class="disclaimer-text">
           By signing up, you agree to the
           <span style="color: #42c2ff">Terms of Service</span> and
@@ -24,6 +38,7 @@
 </template>
 
 <script>
+import '../assets/main.css'
     export default {
         
     }
