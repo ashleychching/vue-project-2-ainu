c2d2af21d02c2a883243a2b749771fb0c90e6610
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-03-31 13:58:32 -0400

    dd

--- project/src/components/navbar.vue
+++ project/src/components/navbar.vue
@@ -1,21 +1,32 @@
 <template>
   <nav>
-    <div>
-      <router-link to="/"><img src="../assets/logo.svg" alt="" /></router-link>
-    </div>
-    <div>
-      <input type="text" placeholder="Search.." />
-    </div>
-    <div>
-      <router-link to="/"><img src="../assets/bookmark.svg" alt="" /></router-link>
-    </div>
-    <div>
-      <button>Logout</button>
-    </div>
-
-    <div>
-      <router-link to="/login">Login</router-link>
-      <router-link to="/signup">Signup</router-link>
-    </div>
+    <ul>
+      <li><img class="logo" src="../assets/logo.svg" alt="" /></li>
+      <li><input class="search" type="text" placeholder="Search.." /></li>
+      <li><img class= "bookmark" src="../assets/bookmark.svg" alt="" /></li>
+    </ul>
   </nav>
 </template>
+<style>
+nav{
+  margin: 3rem;
+}
+ul{
+  display: block;
+  color: white;
+  text-align: center;
+  display: flex;
+  justify-content: space-evenly;
+  text-decoration: none;
+  height: 5rem;
+}
+.search{
+ color: rgb(13, 77, 13);
+ background-color: white;
+ height: 4rem;
+ border-radius: 6px;
+}
+.bookmark, .logo{
+  height: 5rem;
+}
+</style>
