44ab5a4388ed9872917d89f62e0048182482a7ef
darren <90792604+darrenh6@users.noreply.github.com>
2022-03-31 13:58:38 -0400

    Update MainApp.vue
    wwwwwwwww

--- project/src/components/MainApp.vue
+++ project/src/components/MainApp.vue
@@ -1,8 +1,30 @@
 <template>
-  <div>
-    <img src="../assets/logo.svg" alt="" srcset="" />
-    <input type="text" placeholder="Search…" />
-  </div>
+  <main>
+    <nav class="nbar">
+      <ul>
+        <p style="color: #fff">hi</p>
+        <li><img src="../assets/logo.svg" alt="" class="bar-logo" /></li>
+        <li><input type="text" placeholder="Search…" class="bar-search" /></li>
+        <li>
+          <img
+            src="../assets/favorited_false.svg"
+            alt=""
+            class="bar-favorited"
+          />
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
 <script></script>
-<style></style>
+<style>
+.nbar {
+  display: flex;
+}
+
+.bar-logo,
+.bar-search,
+.bar-favorited {
+  height: 5rem;
+}
+</style>
