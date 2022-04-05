89ab4a10ab4fa9e0da1204a8be06cff375077ef0
darren <90792604+darrenh6@users.noreply.github.com>
2022-04-05 13:58:37 -0400

    ok

--- project/src/components/MainApp.vue
+++ project/src/components/MainApp.vue
@@ -1,30 +1,71 @@
 <template>
   <main>
-    <nav class="nbar">
-      <ul>
-        <p style="color: #fff">hi</p>
-        <li><img src="../assets/logo.svg" alt="" class="bar-logo" /></li>
-        <li><input type="text" placeholder="Search…" class="bar-search" /></li>
-        <li>
+    <nav>
+      <ul class="nbar">
+        <li class="nbar-l">
+          <img src="../assets/logo.svg" alt="" class="bar-logo" />
+        </li>
+        <li class="nbar-l">
+          <input type="text" placeholder="&#xF002;" class="bar-search" />
+        </li>
+        <li class="nbar-l">
           <img
             src="../assets/favorited_false.svg"
             alt=""
             class="bar-favorited"
           />
         </li>
+        <li class="nbar-lf">
+          <img src="../assets/profile_mask.svg" alt="" class="bar-avi" />
+        </li>
       </ul>
     </nav>
+    <section class="m-content">
+      <div v-if="feed_empty">
+        <img
+          src="https://c.tenor.com/Nv2YauTamwIAAAAC/dead-chat-dead.gif"
+          alt="dead chat"
+        />
+      </div>
+    </section>
   </main>
 </template>
-<script></script>
+<script>
+export default {
+  name: "App",
+};
+</script>
 <style>
+nav {
+  display: flex;
+  justify-content: center;
+}
+
 .nbar {
   display: flex;
+  justify-content: space-evenly;
+}
+
+.nbar-l {
+  margin-left: 3rem;
+  margin-right: 3rem;
+}
+
+.nbar-lf {
+  margin-left: 1.5rem;
+  margin-right: 3rem;
 }
 
 .bar-logo,
 .bar-search,
-.bar-favorited {
+.bar-favorited,
+.bar-avi {
   height: 5rem;
+}
+
+.bar-search {
+  width: 40vw;
+  font-family: Arial, FontAwesome;
+  text-indent: 1.6rem;
 }
 </style>
