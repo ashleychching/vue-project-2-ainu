8e559b236b3a9faa9f7bc2faa631101976248b26
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-04-26 13:55:18 -0400

    c

---
+++ project/src/components/upload.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+    export default {
+        name: 'upload',
+    }
+</script>
+
+<style >
+
+</style>
