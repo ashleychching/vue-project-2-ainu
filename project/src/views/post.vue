ab55c4a8a48faade0603e948611d34b659629ae9
darrenh6 <90792417+ashleychchan@users.noreply.github.com>
2022-05-05 13:55:59 -0400

    dwd

--- project/src/views/post.vue
+++ project/src/views/post.vue
@@ -1,23 +1,67 @@
 <template>
-    <div>
-  <form action="/action_page.php">
-  <label for="fname">Name of Animal:</label><br>
-  <input type="text" id="fname" name="name"><br>
-  <label for="lname">Age:</label><br>
-  <input type="text" id="lname" name="age"><br><br>
-  <label for="lname">Located in:</label><br>
-  <input type="text" id="lname" name="location"><br><br>
-  <input type="submit" value="Submit">
-</form> 
-    </div>
+  <div>
+    <form @submit.prevent="onCreateMeetup" action="/action_page.php">
+      <label for="fname">Name of Animal:</label><br />
+      <input
+        name="name"
+        label="Name"
+        id="name"
+        v-model="name"
+        required
+      /><br />
+      <label for="lname">Age:</label><br />
+      <input
+        name="age" 
+        label="Age"
+        id="age"
+        v-model="age"
+        required
+      /><br />
+      <label for="lname">Located in:</label><br />
+       <input
+        name="location"
+        label="Location"
+        id="location"
+        v-model="location"
+        required
+      /><br />
+      <label for="lname">Description:</label><br />
+       <input
+        name="description"
+        label="Description"
+        id="description"
+        v-model="description"
+        required
+      /><br />
+      <input type="submit" value="Submit" />
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+    data(){
+        return{
+            name:'',
+            age: '',
+            location:'',
+            description:'',
+
+        }
+    },
+    computed: {
+      formIsValid () {
+        return this.name !== '' &&
+          this.age !== '' &&
+          this.location !== '' &&
+          this.description !== ''
+      },
+    methods: {
+      onCreateMeetup () {
+        if (!this.formIsValid) {
+          return
+        }}}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
