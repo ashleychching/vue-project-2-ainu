cf7f7f2bb6ca3ca84f20e8dde32264bd50d44ff1
darren <yun@crime.su>
2022-05-17 13:40:33 -0400

    f

--- project/src/store/index.js
+++ project/src/store/index.js
@@ -2,7 +2,10 @@ import { createStore } from "vuex";
 
 // firebase imports
 import { auth } from "../firebase/config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const store = createStore({
   state: {
@@ -18,12 +21,23 @@ const store = createStore({
     async signup(context, { email, password }) {
       console.log("signup action");
 
-      // async code
       const res = await createUserWithEmailAndPassword(auth, email, password);
+
       if (res) {
-        context.commit("setUser", res.user)
+        context.commit("setUser", res.user);
       } else {
-        throw new Error("could not complete signup")
+        throw new Error("could not complete signup");
+      }
+    },
+    async login(context, { email, password }) {
+      console.log("login action");
+
+      const res = await signInWithEmailAndPassword(auth, email, password);
+
+      if (res) {
+        context.commit("setUser", res.user);
+      } else {
+        throw new Error("could not complete login");
       }
     },
   },
